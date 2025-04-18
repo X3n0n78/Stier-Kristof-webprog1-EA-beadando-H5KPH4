34f42d9ee70161471c4333235118cff80f9e8f76
X3n0n78 <31189580+X3n0n78@users.noreply.github.com>
2025-04-18 15:58:50 +0200

    teszt3

--- react-projekt/webpack.conf.js
+++ react-projekt/webpack.conf.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "development", // VAGY "production"
-  entry: "./src/react.js",
+  entry: "./src/index.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
