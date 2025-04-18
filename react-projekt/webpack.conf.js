90a7ebdc604c569d5049ebc131f492b3cdb1922b
X3n0n78 <31189580+X3n0n78@users.noreply.github.com>
2025-04-18 15:43:42 +0200

    Teljes frissítés react

---
+++ react-projekt/webpack.conf.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/react.html"
+    })
+  ],
+  devServer: {
+    static: "./dist",
+    port: 8080,
+    open: true
+  }
+};
