c8eadffd14e631824d8ab40d1d000e7924a29773
X3n0n78 <31189580+X3n0n78@users.noreply.github.com>
2025-04-18 16:00:14 +0200

    teszt4

--- react-projekt/src/index.js
+++ react-projekt/src/index.js
@@ -1,7 +1,6 @@
-import React from "react";
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App_temp";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
